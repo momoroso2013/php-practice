cfdab6c1864340a2a03b67e7fe72974110ab0797
Keita Momotani <keita.momotani@gmail.com>
2018-01-06 22:58:35 +0900

    index作成

--- practice-1/input.php
+++ practice-1/input.php
@@ -12,8 +12,21 @@
     <script src="/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container-fluid">
-<button type="button" class="btn btn-default">Default</button>
-<button type="button" class="btn btn-primary">Primary</button>
-</div>
+    <h3 class="text-info">MEMO</h1>
+    <form action="confirm.php" method="POST" class="form-group">
+        <label>Title</label>
+        <input type="text" name="title" class="form-control col-2">
+        <label>Time</label>
+        <input type="date" name="date" class="form-control col-2">
+        <label>Jerne</label>
+        <select name="jerne" class="form-control col-2">
+            <option value="1">うれしい</option>
+            <option value="2">かなしい</option>
+            <option value="3">たのしい</option>
+            <option value="4">はらだたしい</option>
+        </select>
+        <label>Text</label>
+        <textarea name="text" class="form-control col-3" placeholder="text"></textarea>
+        <input type="submit" value="登録" class="btnbtn-default">
+    </form>
 </body>
