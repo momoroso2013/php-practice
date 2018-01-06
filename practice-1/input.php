626f28aea1f5bf04ffe32bbebc8cff5de20a8239
Keita Momotani <keita.momotani@gmail.com>
2018-01-06 21:14:26 +0900

    practice開始

---
+++ practice-1/input.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+ <head>
+    <meta charset="utf-8">
+    <!-- BootstrapのCSS読み込み -->
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery読み込み -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+<button type="button" class="btn btn-default">Default</button>
+<button type="button" class="btn btn-primary">Primary</button>
+</div>
+</body>
