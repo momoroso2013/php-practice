df6dd3e1ca8698f691dce60816975953b47220a5
Keita Momotani <keita.momotani@gmail.com>
2018-01-06 21:13:31 +0900

    indexページの作成

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <head>
+    <meta charset="utf-8">
+    <!-- BootstrapのCSS読み込み -->
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery読み込み -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+</head>
+    <table class="table table-condensed">
+        <tr>
+            <th>Practice<th>
+        </tr>
+        <tr>
+            <td><a href="/practice-1/input.php">practice-1 regidtration</a></td>
+        </tr>
+        <tr>
+            <td><a href="">sample</a></td>
+        </tr>
+    </table>
