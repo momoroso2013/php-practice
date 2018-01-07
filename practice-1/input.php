316c59cf0ba0f387c7783062cfd9b1a86e9f8971
Keita Momotani <keita.momotani@gmail.com>
2018-01-08 00:28:32 +0900

    confirmから戻ってきた時の処理の追加

--- practice-1/input.php
+++ practice-1/input.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
 
+session_start();
+
+$genre_list = [
+    1 => "うれしい",
+    2 => "かなしい",
+    3 => "たのしい",
+    4 => "はらだたしい",
+    5 => "その他"
+    ]
 ?>
 <!DOCTYPE html>
  <head>
@@ -15,18 +24,17 @@
     <h3 class="text-info">MEMO</h1>
     <form action="confirm.php" method="POST" class="form-group">
         <label>Title</label>
-        <input type="text" name="title" class="form-control col-2">
+        <input type="text" name="title" value="<?php echo isset($_SESSION['title']) ? $_SESSION['title'] : "" ?>" class="form-control col-2">
         <label>Time</label>
-        <input type="date" name="date" class="form-control col-2">
+        <input type="date" name="date" value="<?php echo isset($_SESSION['date']) ? $_SESSION['date'] : "" ?>"class="form-control col-2">
         <label>Jerne</label>
-        <select name="jerne" class="form-control col-2">
-            <option value="1">うれしい</option>
-            <option value="2">かなしい</option>
-            <option value="3">たのしい</option>
-            <option value="4">はらだたしい</option>
+        <select name="genre_number" class="form-control col-2">
+            <?php foreach($genre_list as $genre_number => $genre): ?>
+            <option value="<?php echo $genre_number ?>" <?php echo $genre_number == $_SESSION["genre_number"] ? "selected" : ""?>> <?php echo $genre?></option>
+            <?php endforeach; ?>
         </select>
         <label>Text</label>
-        <textarea name="text" class="form-control col-3" placeholder="text"></textarea>
-        <input type="submit" value="登録" class="btnbtn-default">
+        <textarea name="text" class="form-control col-3" rows="4" placeholder="何があったの？"><?php echo isset($_SESSION['text']) ? $_SESSION['text'] : "" ?></textarea>
+        <input type="submit" value="確認" class="btn btn-success">
     </form>
 </body>
